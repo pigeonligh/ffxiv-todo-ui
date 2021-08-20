9988a0ce9a3ccd7631ba88a309589625ef2bb2ea
pigeonligh <pigeonligh@hotmail.com>
2021-08-21 03:07:44 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+<div>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+</div>
 </template>
 
 <script>
@@ -16,11 +17,17 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  background-color: var(--surface-a);
+  font-family: var(--font-family);
+  font-weight: 400;
+  color: var(--text-color);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/arya-orange/theme.css'; // theme
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount('#app');
